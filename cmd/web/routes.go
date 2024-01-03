8d6f46349402c11a4c065a3230d9669378739724
Amr Ojjeh <amrojjeh@outlook.com>
2024-01-02 19:50:43 -0600

    Embed static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,13 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/amrojjeh/yaag/ui"
+)
 
 func (app application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", app.homeGet())
-	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(
-		http.Dir("./ui/static/"))))
+	mux.Handle("/static/", http.FileServer(http.FS(ui.Files)))
 
 	return mux
 }
